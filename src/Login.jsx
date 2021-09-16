d8826a59bf438b3a94607df1f0f0542236b3874c
m1172 <muhshuh2000@gmail.com>
2021-09-16 22:44:28 +0200

    edited

--- src/Login.jsx
+++ src/Login.jsx
@@ -10,6 +10,7 @@ export default class Login extends Component {
         <Container>
           <Head>
             <Body>
+              <div></div>
               <img src='' alt='' />
               <h1>Welcome Back</h1>
               <h4>Login back into your account</h4>
